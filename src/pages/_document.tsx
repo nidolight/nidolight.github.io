15646f53928d75700110d04d5fd011e3dcde2091
nidolight <nidolight@outlook.com>
2025-02-03 12:08:25 +0900

    fix:_documnet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,51 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+    DocumentContext,
+    DocumentInitialProps,
+    Head,
+    Html,
+    Main,
+    NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta charSet="UTF-8" />
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-    );
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta charSet="UTF-8" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
